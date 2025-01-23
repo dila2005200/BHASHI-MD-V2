af3fa1396d05295335c43956fe2f2fdb748de0f2
dila2005200 <dilshandilshan20089976337@gmail.com>
2025-01-23 19:50:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
   SESSION_ID: process.env.SESSION_ID || "",
 // For extra thing : BHASHI-MD~(megafileid)
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb+srv://eshanrashmika2007:<db_Eshan2007@>@cluster0.x92kh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 
     
  
